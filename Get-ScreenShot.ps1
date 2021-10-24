0fbb4705f39692584d8d800ddd5cc8da994fe767
Sergey Poletaev <67954869+spoletaev71@users.noreply.github.com>
2021-10-24 17:14:19 +0500

    Add files via upload

--- Get-ScreenShot.ps1
+++ Get-ScreenShot.ps1
@@ -1,50 +1,41 @@
 ﻿function Get-ScreenShot {
 <#
 .SYNOPSIS
-Делает снимок экрана как картинку в папку.
+Делает снимок экрана как картинку в текущую папку.
 
 .DESCRIPTION
-Делает снимок экрана как картинку в папку.
+Делает снимок экрана как картинку в текущую папку.
 
-.PARAMETER $ComputerName
-Задает имя проверяемого компьютера. Не обязательный параметр.
-По умолчанию: текущий компьютер
 
 .EXAMPLE
-Screenshot.ps1
+Get-Screenshot
 Делает снимок экрана как картинку в папку.
-
-.EXAMPLE
-Get-ADComputer -Filter * -SearchBase "ou=DomainComputers,dc=domain,dc=ru" | %{./Screenshot.ps1 $_.name}
-Делает снимок экрана как картинку в папку для компьютеров из контейнера AD "ou=DomainComputers,dc=domain,dc=ru".
 
 .NOTES
-ИМЯ: Screenshot.ps1
+ИМЯ: Get-Screenshot.ps1
 ЯЗЫК: PoSH
 ДАТА СОЗДАНИЯ: 30.05.2018
 #>
 
 [CmdletBinding()]
 
-Param (
-    [Parameter (Mandatory = $false, Position = 0)]
-    [String]$ComputerName = 'LOCALHOST'
-)
+Param ()
 
 Process {
     [string]$Path = Get-Location
 
-    if ($ComputerName = 'LOCALHOST') { $ComputerName = $env:COMPUTERNAME }
-    
-    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
-	    [void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-    	$size = [Windows.Forms.SystemInformation]::VirtualScreen
-	    $bitmap = new-object Drawing.Bitmap $size.width, $size.height
-    	$graphics = [Drawing.Graphics]::FromImage($bitmap)
-	    $graphics.CopyFromScreen($size.location,[Drawing.Point]::Empty, $size.size)
-    	$graphics.Dispose()
-	    $bitmap.Save("$Path\scr-$ComputerName.png")
-    	$bitmap.Dispose()
-    }
+    [void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    $size = [Windows.Forms.SystemInformation]::VirtualScreen
+
+    $bitmap = new-object Drawing.Bitmap $size.width, $size.height
+
+    $graphics = [Drawing.Graphics]::FromImage($bitmap)
+    $graphics.CopyFromScreen($size.location, [Drawing.Point]::Empty, $size.size)
+    $graphics.Dispose()
+
+    $bitmap.Save("$Path\scr-$ComputerName.png")
+    $bitmap.Dispose()
+
 }#Process
+
 }#function
